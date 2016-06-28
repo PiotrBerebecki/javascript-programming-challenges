c2b1eb20e7792f9fe31f7b2faa1f90b72b8ba834
PiotrBerebecki <petebere@gmail.com>
2016-06-28 18:34:42 +0100

    Add 6-kyu-repalce-you-with-your-sister kata

---
+++ codewars-code-katas/6-kyu-repalce-you-with-your-sister.js
@@ -0,0 +1,45 @@
+console.clear();
+
+/*
+-----------------------------------------
+6 kyu
+Repalce you with your sister (aka Evil Autocorrect Prank)
+-----------------------------------------
+
+Replaces all instances of "you" or "u" (not case sensitive) 
+with "your sister" (always lower case).
+For the purposes of this kata, here's what you need to replace:
+
+"youuuuu" with any number of u characters tacked onto the end
+ 
+"u" at the beginning, middle, or end of a string, but NOT part of a word
+
+"you" but NOT as part of another word like youtube or bayou
+
+*/
+
+
+
+// --------------------------------------
+// regex
+function autocorrect(input) {
+  return input.replace(/\b(you+|u)\b/gi, 'your sister');
+}
+// \b is for word boundary
+
+console.log(  autocorrect('I will meet you when I see u.')  );
+
+
+
+// --------------------------------------
+function autocorrect2(input) {
+  return input.replace(/(^|\W)(you+|u)(?=\W|$)/gi, '$1your sister');
+}
+  
+/*
+\W matches any non-word character [^a-zA-Z0-9_]
+JavaScript supports lookaheads but
+like (?=…)  (positive) and (?!…)  (negative)
+but lacks support for lookbehinds
+like (?<=…) (positive) and (?<!…) (negative)
+*/
