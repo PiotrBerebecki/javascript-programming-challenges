7a897bd16ca551e9f1fc7d3514c960635a4a6cbd
PiotrBerebecki <petebere@gmail.com>
2016-08-04 13:39:47 +0100

    Update the Exact Change formatting

--- freecodecamp-algorithms/advanced-algorithms-exact-change.js
+++ freecodecamp-algorithms/advanced-algorithms-exact-change.js
@@ -62,20 +62,28 @@ function checkCashRegister(price, cash, cid) {
 }
 
 console.log(  JSON.stringify(checkCashRegister(0.04, 0.20, [['PENNY', 0.01], ['NICKEL', 0.05], ['DIME', 0.20]]))  );
-// should return should return [['DIME',0.1],['NICKEL',0.05],['PENNY',0.01]].
+  // should return should return [['DIME',0.1],['NICKEL',0.05],['PENNY',0.01]].
 
-// console.log(  JSON.stringify(checkCashRegister(19.50, 20.00, [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.10], ['QUARTER', 4.25], ['ONE', 90.00], ['FIVE', 55.00], ['TEN', 20.00], ['TWENTY', 60.00], ['ONE HUNDRED', 100.00]]))  )  // should return an array.
+// console.log(  JSON.stringify(checkCashRegister(19.50, 20.00, [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.10], ['QUARTER', 4.25], ['ONE', 90.00], ['FIVE', 55.00], ['TEN', 20.00], ['TWENTY', 60.00], ['ONE HUNDRED', 100.00]]))  )  
+  // should return an array.
 
-// console.log(  JSON.stringify(checkCashRegister(19.50, 20.00, [['PENNY', 0.01], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]))  )  // should return a string.
+// console.log(  JSON.stringify(checkCashRegister(19.50, 20.00, [['PENNY', 0.01], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]))  )  
+  // should return a string.
 
-// console.log(  JSON.stringify(checkCashRegister(19.50, 20.00, [['PENNY', 0.50], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]))  )  // should return a string.
+// console.log(  JSON.stringify(checkCashRegister(19.50, 20.00, [['PENNY', 0.50], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]))  )  
+  // should return a string.
 
-// console.log(  JSON.stringify(checkCashRegister(19.50, 20.00, [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.10], ['QUARTER', 4.25], ['ONE', 90.00], ['FIVE', 55.00], ['TEN', 20.00], ['TWENTY', 60.00], ['ONE HUNDRED', 100.00]]))  )  // should return [['QUARTER', 0.50]].
+// console.log(  JSON.stringify(checkCashRegister(19.50, 20.00, [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.10], ['QUARTER', 4.25], ['ONE', 90.00], ['FIVE', 55.00], ['TEN', 20.00], ['TWENTY', 60.00], ['ONE HUNDRED', 100.00]]))  )  
+  // should return [['QUARTER', 0.50]].
 
-// console.log(  JSON.stringify(checkCashRegister(3.26, 100.00, [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.10], ['QUARTER', 4.25], ['ONE', 90.00], ['FIVE', 55.00], ['TEN', 20.00], ['TWENTY', 60.00], ['ONE HUNDRED', 100.00]]))  )  // should return [['TWENTY', 60.00], ['TEN', 20.00], ['FIVE', 15.00], ['ONE', 1.00], ['QUARTER', 0.50], ['DIME', 0.20], ['PENNY', 0.04]].
+// console.log(  JSON.stringify(checkCashRegister(3.26, 100.00, [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.10], ['QUARTER', 4.25], ['ONE', 90.00], ['FIVE', 55.00], ['TEN', 20.00], ['TWENTY', 60.00], ['ONE HUNDRED', 100.00]]))  )  
+  // should return [['TWENTY', 60.00], ['TEN', 20.00], ['FIVE', 15.00], ['ONE', 1.00], ['QUARTER', 0.50], ['DIME', 0.20], ['PENNY', 0.04]].
 
-// console.log(  JSON.stringify(checkCashRegister(19.50, 20.00, [['PENNY', 0.01], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]))  )  // should return 'Insufficient Funds'.
+// console.log(  JSON.stringify(checkCashRegister(19.50, 20.00, [['PENNY', 0.01], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]))  )  
+  // should return 'Insufficient Funds'.
 
-// console.log(  JSON.stringify(checkCashRegister(19.50, 20.00, [['PENNY', 0.01], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 1.00], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]))  )  // should return 'Insufficient Funds'.
+// console.log(  JSON.stringify(checkCashRegister(19.50, 20.00, [['PENNY', 0.01], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 1.00], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]))  )  
+  // should return 'Insufficient Funds'.
 
-// console.log(  JSON.stringify(checkCashRegister(19.50, 20.00, [['PENNY', 0.50], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]))  )  // should return 'Closed'.
+// console.log(  JSON.stringify(checkCashRegister(19.50, 20.00, [['PENNY', 0.50], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]))  )  
+  // should return 'Closed'.
