9959839c05e3cdf1ae65eba143f391d0fd8df2d4
PiotrBerebecki <petebere@gmail.com>
2017-04-19 11:55:56 +0100

    Add pipe and compose functions

---
+++ snippets/pipe-and-compose.js
@@ -0,0 +1,33 @@
+const addThree = (a, b, c) => a + b + c;
+const dbl = a => a * 2;
+const toCurrency = num => `£${num}`;
+
+// ****************************************************************
+// pipe
+// ****************************************************************
+
+// in two steps
+const _pipe = (f, g) => (...args) => g(f(...args));
+const pipe = (...fns) => fns.reduce(_pipe);
+
+// in one step
+const pipe = (fnFirst, ...fns) => (...args) =>
+  fns.reduce((acc, fn) => fn(acc), fnFirst(...args));
+
+const addThreeAndDouble = pipe(addThree, dbl)(1, 2, 3);
+console.log(addThreeAndDouble); // 12
+
+// ****************************************************************
+// compose
+// ****************************************************************
+
+// in two steps
+const _compose = (f, g) => (...args) => f(g(...args));
+const compose = (...fns) => fns.reduce(_compose);
+
+// in one step
+const compose = (...fns) => (...args) =>
+  fns.slice(0, -1).reduceRight((acc, fn) => fn(acc), fns.slice(-1)[0](...args));
+
+const addThreeAndToCurrency = compose(toCurrency, addThree)(1, 2, 3);
+console.log(addThreeAndToCurrency); // £6
