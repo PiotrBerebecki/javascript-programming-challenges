34e31f274401e3cc18a6758febc876de5a93a57b
Piotr Berebecki <petebere@gmail.com>
2017-06-02 01:00:57 +0100

    Create 8-kyu-fill-array-with-consecutive-numbers.js

---
+++ codewars-code-katas/8-kyu-fill-array-with-consecutive-numbers.js
@@ -0,0 +1,10 @@
+// For example, if n = 10:
+// return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function monkeyCount(n) {
+  return Array.from({length: n}, (val, idx) => idx + 1);
+}
+
+function monkeyCount2(n) {
+  return Array.from(new Array(n), (_, idx) => idx + 1);
+}
