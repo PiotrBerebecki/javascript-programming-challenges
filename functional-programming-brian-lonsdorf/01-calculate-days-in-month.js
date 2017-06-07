fb55f4866f6e695dbd495b362bfa85cb554e1e21
Piotr Berebecki <petebere@gmail.com>
2017-06-07 23:22:14 +0100

    Create 01-calculate-days-in-month.js

---
+++ functional-programming-brian-lonsdorf/01-calculate-days-in-month.js
@@ -0,0 +1,22 @@
+// https://codepen.io/PiotrBerebecki/pen/zzvNeG?editors=0010
+// calculate days in the current month
+
+function daysInMonth(y, m) {
+  const start = new Date(y, m - 1, 1);
+  const end = new Date(y, m, 1);
+  return (end - start) / (1000 * 60 * 60 * 24);
+}
+
+console.log( daysInMonth(2017, 2 ));
+
+// the above is much easier to test than the below
+// which relies on time
+
+function daysThisMonth2() {
+  var date  = new Date(),
+    y     = date.getFullYear(),
+    m     = date.getMonth(),
+    start = new Date(y, m, 1),
+    end   = new Date(y, m + 1, 1);
+  return (end - start) / (1000 * 60 * 60 * 24);
+}
